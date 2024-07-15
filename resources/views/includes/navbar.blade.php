4bb98cbe1329faac533c1ab1e5fc38600099c3a4
divawidia <wiartha2001@gmail.com>
2024-07-15 15:09:44 +0800

    fix page bugs

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -11,6 +11,7 @@
             type="button"
             data-toggle="collapse"
             data-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"
         >
             <span class="navbar-toggler-icon"></span>
         </button>
