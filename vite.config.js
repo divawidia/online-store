9184e9e8b36560fbc0fc23cf933e370773216bc7
divawidia <wiartha2001@gmail.com>
2025-03-04 00:00:00 +0000

    feat (vite config): add config to prevent CORS

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: 'localhost',
+        cors: {
+            origin: 'http://online-store.test',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization']
+        }
+    },
     plugins: [
         laravel({
             input: [
