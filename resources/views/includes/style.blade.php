af2417e1b7f16970b26aa1f3877952b9676a2854
divawidia <wiartha2001@gmail.com>
2025-03-04 00:00:00 +0000

    refactor (styles includes): delete unecessary package

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,8 +1,2 @@
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-<link href="/style/main.css" rel="stylesheet" />
-<link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
-    crossorigin="anonymous"
-/>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.21/datatables.min.css"/>
