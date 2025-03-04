da490d99501ca16d4327fc0ac26a1c5b41e0bff3
divawidia <wiartha2001@gmail.com>
2025-03-04 00:00:00 +0000

    feat (app js): add navbar and sidebar functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+$(function () {
+    $(document).scroll(function () {
+        const $nav = $(".navbar-fixed-top");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+});
