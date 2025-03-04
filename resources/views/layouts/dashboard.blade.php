e35acfe2e2de83464e2660f24b27005ec7df43d7
divawidia <wiartha2001@gmail.com>
2025-03-04 00:00:00 +0000

    feat (dashboard layout): add vite assets

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
+@php use App\Models\Cart;use Illuminate\Support\Facades\Auth; @endphp
+    <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
 
     <title>@yield('title')</title>
 
     @stack('prepend-style')
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="/style/main.css" rel="stylesheet" />
+    @include('includes.style')
     @stack('addon-style')
+    @vite(['resources/sass/app.scss', 'resources/js/app.js',])
 </head>
 
 <body>
@@ -23,44 +21,26 @@
         <!-- Sidebar -->
         <div class="border-right" id="sidebar-wrapper">
             <div class="sidebar-heading text-center">
-                <img src="/images/dashboard-store-logo.svg" alt="" class="my-4" />
+                <img src="{{asset('/images/dashboard-store-logo.svg')}}" alt="logo" class="my-4" width="50"/>
             </div>
             <div class="list-group list-group-flush">
-                <a
-                    href="{{ route('dashboard') }}"
-                    class="list-group-item list-group-item-action {{ (request()->is('dashboard')) ? 'active' : '' }} "
-                >
+                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-action {{ (request()->is('dashboard')) ? 'active' : '' }} ">
                     Dashboard
                 </a>
-                <a
-                    href="{{ route('dashboard-product') }}"
-                    class="list-group-item list-group-item-action {{ (request()->is('dashboard/products*')) ? 'active' : '' }} "
-                >
+                <a href="{{ route('dashboard-product') }}" class="list-group-item list-group-item-action {{ (request()->is('dashboard/products*')) ? 'active' : '' }} ">
                     My Products
                 </a>
-                <a
-                    href="{{ route('dashboard-transaction') }}"
-                    class="list-group-item list-group-item-action {{ (request()->is('dashboard/transactions*')) ? 'active' : '' }} "
-                >
+                <a href="{{ route('dashboard-transaction') }}" class="list-group-item list-group-item-action {{ (request()->is('dashboard/transactions*')) ? 'active' : '' }} ">
                     Transactions
                 </a>
-                <a
-                    href="{{ route('dashboard-settings-store') }}"
-                    class="list-group-item list-group-item-action {{ (request()->is('dashboard/settings*')) ? 'active' : '' }} "
-                >
+                <a href="{{ route('dashboard-settings-store') }}" class="list-group-item list-group-item-action {{ (request()->is('dashboard/settings*')) ? 'active' : '' }} ">
                     Store Settings
                 </a>
-                <a
-                    href="{{ route('dashboard-settings-account') }}"
-                    class="list-group-item list-group-item-action {{ (request()->is('dashboard/account*')) ? 'active' : '' }} "
-                >
+                <a href="{{ route('dashboard-settings-account') }}" class="list-group-item list-group-item-action {{ (request()->is('dashboard/account*')) ? 'active' : '' }} ">
                     My Account
                 </a>
-                <a
-                    href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();"
-                    class="list-group-item list-group-item-action"
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                   class="list-group-item list-group-item-action"
                 >
                     Sign Out
                 </a>
@@ -72,41 +52,20 @@
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
-            <nav
-                class="navbar navbar-expand-lg navbar-light navbar-store fixed-top"
-                data-aos="fade-down"
-            >
+            <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top" data-aos="fade-down">
                 <div class="container-fluid">
-                    <button
-                        class="btn btn-secondary d-md-none mr-auto mr-2"
-                        id="menu-toggle"
-                    >
+                    <button class="btn btn-secondary d-md-none mr-auto mr-2" id="menu-toggle">
                         &laquo; Menu
                     </button>
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                    >
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Desktop Menu -->
                         <ul class="navbar-nav d-none d-lg-flex ml-auto">
                             <li class="nav-item dropdown">
-                                <a
-                                    href="#"
-                                    class="nav-link"
-                                    id="navbarDropdown"
-                                    role="button"
-                                    data-toggle="dropdown"
-                                >
-                                    <img
-                                        src="/images/icon-user.png"
-                                        alt=""
-                                        class="rounded-circle mr-2 profile-picture"
-                                    />
+                                <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown">
+                                    <img src="{{asset('/images/icon-user.png')}}" alt="user" class="rounded-circle mr-2 profile-picture"/>
                                     Hi, {{ Auth::user()->name }}
                                 </a>
                                 <div class="dropdown-menu">
@@ -115,8 +74,7 @@
                                         Settings
                                     </a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         Logout
                                     </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -127,13 +85,13 @@
                             <li class="nav-item">
                                 <a href="{{ route('cart') }}" class="nav-link d-inline-block mt-2">
                                     @php
-                                        $carts = \App\Models\Cart::where('users_id', Auth::user()->id)->count();
+                                        $carts = Cart::where('users_id', Auth::user()->id)->count();
                                     @endphp
                                     @if($carts > 0)
-                                        <img src="/images/icon-cart-filled.svg" alt="" />
+                                        <img src="{{asset('/images/icon-cart-filled.svg')}}" alt=""/>
                                         <div class="card-badge">{{ $carts }}</div>
                                     @else
-                                        <img src="/images/icon-cart-empty.svg" alt="" />
+                                        <img src="{{asset('/images/icon-cart-empty.svg')}}" alt=""/>
                                     @endif
                                 </a>
                             </li>
@@ -157,25 +115,13 @@
 
             {{-- Content --}}
             @yield('content')
-
         </div>
     </div>
 </div>
 
 <!-- Bootstrap core JavaScript -->
 @stack('prepend-script')
-<script src="/vendor/jquery/jquery.slim.min.js"></script>
-<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script>
-    AOS.init();
-</script>
-<script>
-    $("#menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-</script>
+@include('includes.script')
 @stack('addon-script')
 </body>
 </html>
