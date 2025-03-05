1e402593746dba161e9de348e1a17fe45ada7389
divawidia <wiartha2001@gmail.com>
2025-03-05 00:00:00 +0000

    refactor (navbar auth layout): delete reward menu

--- resources/views/includes/navbar-auth.blade.php
+++ resources/views/includes/navbar-auth.blade.php
@@ -1,17 +1,9 @@
-<nav
-    class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top bg-light"
-    data-aos="fade-down"
->
+<nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top bg-light" data-aos="fade-down">
     <div class="container">
         <a href="{{ route('home') }}" class="navbar-brand">
-            <img src="/images/logo.svg" alt="Logo" />
+            <img src="{{asset('/images/logo.svg')}}" alt="Logo" />
         </a>
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-        >
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -21,9 +13,6 @@
                 </li>
                 <li class="nav-item">
                     <a href="{{  route('categories') }}" class="nav-link">Categories</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Rewards</a>
                 </li>
             </ul>
         </div>
