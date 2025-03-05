3f14f1c19007a2369c4214a68099acf6f684db60
divawidia <wiartha2001@gmail.com>
2025-03-05 00:00:00 +0000

    refactor (navbar layout): update user logo

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,18 +1,9 @@
-<nav
-    class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top bg-light"
-    data-aos="fade-down"
->
+<nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top bg-light" data-aos="fade-down">
     <div class="container">
         <a href="{{ route('home') }}" class="navbar-brand">
-            <img src="/images/logo.svg" alt="Logo" />
+            <img src="{{asset('/images/logo.svg')}}" alt="Logo" />
         </a>
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"
-        >
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -23,39 +14,22 @@
                 <li class="nav-item">
                     <a href="{{ route('categories') }}" class="nav-link">Categories</a>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Rewards</a>
-                </li>
                 @guest
                     <li class="nav-item">
                         <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
                     </li>
                     <li class="nav-item">
-                        <a
-                            href="{{  route('login') }}"
-                            class="btn btn-success nav-link px-4 text-white"
-                        >Sign In</a
-                        >
+                        <a href="{{  route('login') }}" class="btn btn-success nav-link px-4 text-white">Sign In</a>
                     </li>
                 @endguest
             </ul>
 
             @auth
                 <!-- Desktop Menu -->
-                <ul class="navbar-nav d-none d-lg-flex">
+                <ul class="navbar-nav d-none d-lg-flex align-items-center">
                     <li class="nav-item dropdown">
-                        <a
-                            href="#"
-                            class="nav-link"
-                            id="navbarDropdown"
-                            role="button"
-                            data-toggle="dropdown"
-                        >
-                            <img
-                                src="/images/icon-user.png"
-                                alt=""
-                                class="rounded-circle mr-2 profile-picture"
-                            />
+                        <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown">
+                            <img src="{{asset('/images/user-circle.png')}}" alt="profile pic" height="40" class="rounded-circle mr-2 profile-picture"/>
                             Hi, {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu">
@@ -64,8 +38,7 @@
                                 Settings
                             </a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 Logout
                             </a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -79,10 +52,10 @@
                                 $carts = \App\Models\Cart::where('users_id', Auth::user()->id)->count();
                             @endphp
                             @if($carts > 0)
-                                <img src="/images/icon-cart-filled.svg" alt="" />
+                                <img src="/{{asset('images/icon-cart-filled.svg')}}" alt="" />
                                 <div class="card-badge">{{ $carts }}</div>
                             @else
-                                <img src="/images/icon-cart-empty.svg" alt="" />
+                                <img src="{{asset('/images/icon-cart-empty.svg')}}" alt="" />
                             @endif
                         </a>
                     </li>
