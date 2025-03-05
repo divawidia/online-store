71b34d9f68ce0c7226c9a1f01bee3cf01051d93b
divawidia <wiartha2001@gmail.com>
2025-03-05 00:00:00 +0000

    refactor (dashboard page): delete unused tags

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,10 +5,7 @@
 @endsection
 
 @section('content')
-    <div
-        class="section-content section-dashboard-home"
-        data-aos="fade-up"
-    >
+    <div class="section-content section-dashboard-home" data-aos="fade-up">
         <div class="container-fluid">
             <div class="dashboard-heading">
                 <h2 class="dashboard-title">Dashboard</h2>
@@ -44,104 +41,26 @@
                 <div class="row mt-3">
                     <div class="col-12 mt-2">
                         <h5 class="mb-3">Recent Transactions</h5>
+                        @if(count($transaction_data) == 0)
+                            <h6 class="text-muted text-center">There is no transactions at this moment</h6>
+                        @endif
                         @foreach($transaction_data as $trasaction)
-                            <a
-                                href="{{ route('dashboard-transaction-details', $trasaction->id) }}"
-                                class="card card-list d-block"
-                            >
+                            <a href="{{ route('dashboard-transaction-details', $trasaction->id) }}" class="card card-list d-block">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col-md-1">
-                                            <img
-                                                src="{{ Storage::url($trasaction->product->galleries->first()->photos ?? '') }}"
-                                                class="w-75"
-                                            />
+                                            <img src="{{ Storage::url($trasaction->product->galleries->first()->photos ?? '') }}" class="w-75" alt="{{ $trasaction->product->name }}"/>
                                         </div>
                                         <div class="col-md-4">{{ $transaction->product->name ?? '' }}</div>
                                         <div class="col-md-3">{{ $transaction->transaction->user->name ?? '' }}</div>
-                                        <div class="col-md-3">{{ $transaction->created_at ?? '' }}</div>
+                                        <div class="col-md-3">{{ date('d F Y', strtotime($transaction->created_at ?? '')) }}</div>
                                         <div class="col-md-1 d-none d-md-block">
-                                            <img
-                                                src="/images/dashboard-arrow-right.svg"
-                                                alt=""
-                                            />
+                                            <img src="{{asset('/images/dashboard-arrow-right.svg')}}" alt=""/>
                                         </div>
                                     </div>
                                 </div>
                             </a>
                         @endforeach
-                        <a
-                            href="/dashboard-transactions-details.html"
-                            class="card card-list d-block"
-                        >
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-1">
-                                        <img
-                                            src="/images/dashboard-icon-product-1.png"
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div class="col-md-4">Sirup Marjan</div>
-                                    <div class="col-md-3">Dipa Widia</div>
-                                    <div class="col-md-3">30 September 2023</div>
-                                    <div class="col-md-1 d-none d-md-block">
-                                        <img
-                                            src="/images/dashboard-arrow-right.svg"
-                                            alt=""
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a
-                            href="/dashboard-transactions-details.html"
-                            class="card card-list d-block"
-                        >
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-1">
-                                        <img
-                                            src="/images/dashboard-icon-product-2.png"
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div class="col-md-4">Sirup Marjan</div>
-                                    <div class="col-md-3">Dipa Widia</div>
-                                    <div class="col-md-3">30 September 2023</div>
-                                    <div class="col-md-1 d-none d-md-block">
-                                        <img
-                                            src="/images/dashboard-arrow-right.svg"
-                                            alt=""
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a
-                            href="/dashboard-transactions-details.html"
-                            class="card card-list d-block"
-                        >
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-1">
-                                        <img
-                                            src="/images/dashboard-icon-product-3.png"
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div class="col-md-4">Sirup Marjan</div>
-                                    <div class="col-md-3">Dipa Widia</div>
-                                    <div class="col-md-3">30 September 2023</div>
-                                    <div class="col-md-1 d-none d-md-block">
-                                        <img
-                                            src="/images/dashboard-arrow-right.svg"
-                                            alt=""
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
                     </div>
                 </div>
             </div>
