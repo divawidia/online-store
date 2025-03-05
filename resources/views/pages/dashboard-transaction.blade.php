227350bcf67215f09611267113fe2d6ce1e3e22a
divawidia <wiartha2001@gmail.com>
2025-03-05 00:00:00 +0000

    refactor (transaction detail page): update transaction tabs

--- resources/views/pages/dashboard-transaction.blade.php
+++ resources/views/pages/dashboard-transaction.blade.php
@@ -6,10 +6,7 @@
 
 @section('content')
     <!-- Section Content -->
-    <div
-        class="section-content section-dashboard-home"
-        data-aos="fade-up"
-    >
+    <div class="section-content section-dashboard-home" data-aos="fade-up">
         <div class="container-fluid">
             <div class="dashboard-heading">
                 <h2 class="dashboard-title">Transactions</h2>
@@ -20,34 +17,16 @@
             <div class="dashboard-content">
                 <div class="row">
                     <div class="col-12 mt-2">
-                        <ul
-                            class="nav nav-pills mb-3"
-                            id="pills-tab"
-                            role="tablist"
-                        >
+                        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                             <li class="nav-item" role="presentation">
-                                <a
-                                    class="nav-link active"
-                                    id="pills-home-tab"
-                                    data-toggle="pill"
-                                    href="#pills-home"
-                                    role="tab"
-                                    aria-controls="pills-home"
-                                    aria-selected="true"
-                                >Sell Product</a
-                                >
+                                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
+                                    Sell Product
+                                </a>
                             </li>
                             <li class="nav-item" role="presentation">
-                                <a
-                                    class="nav-link"
-                                    id="pills-profile-tab"
-                                    data-toggle="pill"
-                                    href="#pills-profile"
-                                    role="tab"
-                                    aria-controls="pills-profile"
-                                    aria-selected="false"
-                                >Buy Product</a
-                                >
+                                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">
+                                    Buy Product
+                                </a>
                             </li>
                         </ul>
                         <div class="tab-content" id="pills-tabContent">
@@ -58,71 +37,39 @@
                                 aria-labelledby="pills-home-tab"
                             >
                                 @foreach ($sellTransactions as $transaction)
-                                    <a
-                                        href="{{ route('dashboard-transaction-details', $transaction->id) }}"
-                                        class="card card-list d-block"
-                                    >
+                                    <a href="{{ route('dashboard-transaction-details', $transaction->id) }}" class="card card-list">
                                         <div class="card-body">
-                                            <div class="row">
+                                            <div class="row d-flex align-items-center">
                                                 <div class="col-md-1">
-                                                    <img
-                                                        src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}"
-                                                        class="w-50"
-                                                    />
+                                                    <img src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}" class="w-50" alt="{{ $transaction->product->name }}"/>
                                                 </div>
-                                                <div class="col-md-4">
-                                                    {{ $transaction->product->name }}
-                                                </div>
-                                                <div class="col-md-3">
-                                                    {{ $transaction->product->user->store_name }}
-                                                </div>
-                                                <div class="col-md-3">
-                                                    {{ $transaction->created_at }}
-                                                </div>
+                                                <div class="col-md-4">{{ $transaction->product->name }}</div>
+                                                <div class="col-md-3">{{ $transaction->transaction->user->name ?? '' }}</div>
+                                                <div class="col-md-3">{{ date('d F Y', strtotime($transaction->created_at ?? '')) }}</div>
                                                 <div class="col-md-1 d-none d-md-block">
-                                                    <img
-                                                        src="/images/dashboard-arrow-right.svg"
-                                                        alt=""
-                                                    />
+                                                    <img src="{{asset('/images/dashboard-arrow-right.svg')}}" alt=""/>
                                                 </div>
                                             </div>
                                         </div>
                                     </a>
                                 @endforeach
                             </div>
-                            <div
-                                class="tab-pane fade"
-                                id="pills-profile"
-                                role="tabpanel"
-                                aria-labelledby="pills-profile-tab"
-                            >
+                            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
                                 @foreach ($buyTransactions as $transaction)
-                                    <a
-                                        href="{{ route('dashboard-transaction-details', $transaction->id) }}"
-                                        class="card card-list d-block"
-                                    >
+                                    <a href="{{ route('dashboard-transaction-details', $transaction->id) }}" class="card card-list d-block">
                                         <div class="card-body">
                                             <div class="row">
                                                 <div class="col-md-1">
-                                                    <img
-                                                        src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}"
-                                                        class="w-50"
-                                                    />
+                                                    <img src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}" class="w-50" alt=""/>
                                                 </div>
-                                                <div class="col-md-4">
-                                                    {{ $transaction->product->name }}
+                                                <div class="col-md-4">{{ $transaction->product->name }}
                                                 </div>
-                                                <div class="col-md-3">
-                                                    {{ $transaction->product->user->store_name }}
+                                                <div class="col-md-3">{{ $transaction->product->user->store_name }}
                                                 </div>
-                                                <div class="col-md-3">
-                                                    {{ $transaction->created_at }}
+                                                <div class="col-md-3">{{ $transaction->created_at }}
                                                 </div>
                                                 <div class="col-md-1 d-none d-md-block">
-                                                    <img
-                                                        src="/images/dashboard-arrow-right.svg"
-                                                        alt=""
-                                                    />
+                                                    <img src="{{asset('/images/dashboard-arrow-right.svg')}}" alt=""/>
                                                 </div>
                                             </div>
                                         </div>
